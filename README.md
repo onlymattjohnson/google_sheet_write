12a2f6ebf324998b52ebc87ea919a1e4563aab40
Matt Johnson <mjohn23@gmail.com>
2020-03-01 10:04:23 -0500

    Write basic code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Google Sheets Test
 This a test to see if we can write to google sheets and keep formatting.
+
+
+## Setup
+
+1. Turn on [google sheets api](https://developers.google.com/sheets/api/quickstart/python)
+2. Download client config and save as credentials.json
+3. Install google client library
+
+```
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+
+4. On first run of your script, you need to authorize flow by going to the URL that pops up.
---
+++ write_to_sheet.py
@@ -0,0 +1,56 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import random
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1lsbQSm_gsb_CQNOnSuoZ4mZclxorLjsMj3V5CvstO_A'
+SAMPLE_RANGE_NAME = 'Sheet1!A1:F10'
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('sheets', 'v4', credentials=creds)
+
+sheet = service.spreadsheets()
+
+# 10 rows
+# 6 columns
+random_vals = [[random.randint(1,101) for i in range(6)] for j in range(0,10)]
+
+new_values = {
+  'values': random_vals
+}
+update = sheet.values().update(spreadsheetId = SAMPLE_SPREADSHEET_ID,
+                              range = SAMPLE_RANGE_NAME,
+                              valueInputOption='USER_ENTERED',
+                              body = new_values
+).execute()
+
+update = sheet.values().update(spreadsheetId = SAMPLE_SPREADSHEET_ID,
+                              range = 'Sheet1!A1:F1',
+                              valueInputOption = 'USER_ENTERED',
+                              body = {'values': [[f'Field {i}' for i in range(6)]]}
+).execute()
